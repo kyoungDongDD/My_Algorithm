6f15d8fd08169cf09b70f5c322f7dad677f1b558
김경동 <rlarudehd32@naver.com>
2022-02-10 22:25:06 +0900

    Algo:boj 전자레인지 S06P12B207-10162 100점맞았습니다 와아아아 이거 점수로 주는것도 있구나 ㅋㅋ

---
+++ src/main/java/boj_10162.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10162 {
+  public static void main (String[]args) throws IOException {
+    BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+    int k=Integer.parseInt(bf.readLine());
+    int []pushNum=new int [3];
+    pushNum[0]=k/300;
+    k=k%300;
+    pushNum[1]=k/60;
+    k=k%60;
+    pushNum[2]=k/10;
+    k=k%10;
+
+    if(k!=0) System.out.println("-1");
+    else System.out.println(pushNum[0]+" "+pushNum[1]+" "+pushNum[2]);
+  }
+}
