6dd5480f9aef52a3dff6805556a77be64fb842e5
김경동 <rlarudehd32@naver.com>
2022-02-15 01:13:10 +0900

    boj_18108  S06P12B207-

---
+++ BOJ/src/main/java/boj_18108.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_18108 {
+  public static void main(String[]args) throws IOException {
+    BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+    System.out.println(Integer.parseInt(bf.readLine())-543);
+  }
+}
