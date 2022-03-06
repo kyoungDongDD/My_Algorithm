89b9281665a1fac64fcf5eb9a72d19ced05533d7
ddddd <rlarudehd32@naver.com>
2022-03-07 07:35:23 +0900

    S06P12B207- 왜안보내짐?

--- BOJ/src/main/java/boj_1312.java
+++ BOJ/src/main/java/boj_1312.java
@@ -6,14 +6,14 @@ public class boj_1312 {
   public static void main(String[]args) throws IOException {
     BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
     String [] nums = bf.readLine().split(" ");
-    int a= Integer.parseInt(nums[0]);
-    int b= Integer.parseInt(nums[1]);
-    int k= Integer.parseInt(nums[2]);
-    int result=0;
+    int a = Integer.parseInt(nums[0]);
+    int b = Integer.parseInt(nums[1]);
+    int k = Integer.parseInt(nums[2]);
+    int result = 0;
     for(int i=0;i<k+1;i++){
-      result = a/b;
+      result = a / b;
       a %= b;
-      a=a*10;
+      a = a*10;
     }
     System.out.println(result);
   }
