256da29c9f47535080cfb0b6bb1b5f2ea529e5a0
ddddd <rlarudehd32@naver.com>
2022-03-07 07:31:15 +0900

    fix :boj_1312 S06P12B207- hh a

--- BOJ/src/main/java/boj_1312.java
+++ BOJ/src/main/java/boj_1312.java
@@ -6,11 +6,15 @@ public class boj_1312 {
   public static void main(String[]args) throws IOException {
     BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
     String [] nums = bf.readLine().split(" ");
-    float cal_num=Float.parseFloat(nums[0])/Float.parseFloat(nums[1]);
+    int a= Integer.parseInt(nums[0]);
+    int b= Integer.parseInt(nums[1]);
     int k= Integer.parseInt(nums[2]);
-    System.out.println("1 = " + cal_num*Math.pow(10,k));
-    System.out.println("2 = " + (int)(cal_num*Math.pow(10,k-1))*10);
-    int result = (int) (cal_num*Math.pow(10,k) - (int)(cal_num*Math.pow(10,k-1))*10);
-    System.out.println( result);
+    int result=0;
+    for(int i=0;i<k+1;i++){
+      result = a/b;
+      a %= b;
+      a=a*10;
+    }
+    System.out.println(result);
   }
 }
