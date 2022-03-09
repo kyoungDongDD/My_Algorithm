f99bfd14169290004fca318e77bdbffc2f396359
ddddd <rlarudehd32@naver.com>
2022-03-09 23:40:49 +0900

    S06P12B207- 왜안보내짐?

--- BOJ/src/main/java/BOJ_14652.java
+++ BOJ/src/main/java/BOJ_14652.java
@@ -8,9 +8,6 @@ public class BOJ_14652 {
     String[]num = bf.readLine().split(" ");
     int r1=Integer.parseInt(num[2])%Integer.parseInt(num[1]);
     int r2=Integer.parseInt(num[2])/Integer.parseInt(num[1]);
-
     System.out.println(r2 + " " + r1);
-
-
   }
 }
