c5b62993a02853a2de06ddc9a02b02a74cc4cd6a
ddddd <rlarudehd32@naver.com>
2022-04-02 16:05:34 +0900

    S06P12B207-

--- BOJ/src/main/java/test.java
+++ BOJ/src/main/java/test.java
@@ -1,47 +1,31 @@
-import static jdk.nashorn.internal.objects.Global.print;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class test {
-    public static void main(String[] args) {
-      int n = 4;
-      int[] arr = new int [n];
-      for(int i=0;i<n;i++) arr[i]=i;
-      boolean[] visited = new boolean[n];
 
-      for (int i = 1; i <= n; i++) {
-        System.out.println("\n" + n + " 개 중에서 " + i + " 개 뽑기");
-        comb(arr, visited, 0, n, i);
-      }
-    }
+    public static void main(String[] args) throws IOException {
+      BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+      int n=Integer.parseInt(bf.readLine());
+      int count=1;
 
-
-
-    // 재귀 사용
-    // 사용 예시 : comb(arr, visited, 0, n, r)
-    static void comb(int[] arr, boolean[] visited, int depth, int n, int r) {
-      if (r == 0) {
-        print(arr, visited, n);
-        return;
-      }
-
-      if (depth == n) {
-        return;
-      }
-
-      visited[depth] = true;
-      comb(arr, visited, depth + 1, n, r - 1);
-
-      visited[depth] = false;
-      comb(arr, visited, depth + 1, n, r);
-    }
-
-    // 배열 출력
-    static void print(int[] arr, boolean[] visited, int n) {
-      for (int i = 0; i < n; i++) {
-        if (visited[i]) {
-          System.out.print(arr[i] + " ");
+      for(int i=1;i<n/2 +1 ;i++){
+        int sum=i;
+        int j=i+1;
+        while(true){
+          sum+=j;
+          if(sum==n){
+            count++;
+            break;
+          }
+          if(sum>n) break;
+          j++;
         }
+        }
+      System.out.println(count);
       }
-      System.out.println();
+
     }
-  }
+
 
